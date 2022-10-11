6722120de3dc86dde3af8befb90bdd19bdc0a5a9
Thushmi <thushmi.sooriyaarachchi@live.vu.edu.au>
2022-10-11 23:51:23 +1100

    area.py

---
+++ area.py
@@ -0,0 +1,9 @@
+print("Hello, let's calculate the area of your rectangle!")
+
+width = float(input("Please enter the width as measured: "))
+
+height = float(input("Please enter the height as measured: "))
+
+area = (width * height)
+
+print("Area of the rectangle = ", area)
